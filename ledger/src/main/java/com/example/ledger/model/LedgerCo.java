7b795576c6790bab55fe8f9cef08216a705c22f1
Subham Mallick <subham2mallick@gmail.com>
2022-10-01 03:11:11 +0530

    Add files via upload

---
+++ ledger/src/main/java/com/example/ledger/model/LedgerCo.java
@@ -0,0 +1,54 @@
+package com.example.ledger.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class LedgerCo {
+
+    private List<Bank> bankList = new ArrayList<>();
+
+    public void createLoan(String bankName, String personName, int loanAmount, int years, int rate){
+        boolean bankFoundInList = false;
+
+        for(Bank ibank: bankList){
+            if(ibank.getName().equalsIgnoreCase(bankName)){
+                bankFoundInList=true;
+                ibank.createLoan(personName, loanAmount, years, rate);
+                break;
+            }
+        }
+
+        if (!bankFoundInList) {
+            Bank bank = new Bank();
+            bank.setName(bankName);
+            bank.createLoan(personName, loanAmount, years, rate);
+            bankList.add(bank);
+        }
+    }
+
+    public void checkBalance(String bankName, String personName, int emiNumber){
+
+        for(Bank bank: bankList){
+            if(bank.getName().equalsIgnoreCase(bankName)){
+                bank.checkBalance(personName,emiNumber);
+                break;
+            }
+        }
+
+    }
+
+    public void doPayment(String bankName, String personName, int lumpsumAmount, int emiNumber){
+
+        for(Bank bank: bankList){
+            if(bank.getName().equalsIgnoreCase(bankName)){
+                bank.doPayment(personName,lumpsumAmount,emiNumber);
+                break;
+            }
+        }
+
+    }
+
+}
